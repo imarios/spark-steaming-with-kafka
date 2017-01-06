167a320e3220a7c673ef476c0da4d1d1d82d3fba
imarios <iliofotou@gmail.com>
2017-01-05 21:56:54 -0800

    Simplifying Kafka events

--- scripts/auto_producer.sh
+++ scripts/auto_producer.sh
@@ -3,7 +3,7 @@
 source $(dirname $0)/common.sh
 
 if [ $# -eq 2 ]; then
-   yes "Hello World" | pv -qL $2 | $kafka_home/bin/kafka-console-producer.sh --topic $1 --broker-list $all_brokers
+   yes | pv -qL $2 | $kafka_home/bin/kafka-console-producer.sh --topic $1 --broker-list $all_brokers
 else
    echo "Usage: "$(basename $0)" <topic> <rate eg 4,1k>"
 fi
